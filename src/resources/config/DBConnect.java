2f4749060e8afabc153cad3c04ae30950a46dc8e
Pham Duc Toan <toan@gmail.com>
2025-03-13 16:56:01 +0700

    dbconnect

--- src/resources/config/DBConnect.java
+++ src/resources/config/DBConnect.java
@@ -8,6 +8,32 @@ package resources.config;
  *
  * @author phamd
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnect {
-    
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                String dbUser = "sa",
+                        dbPass = "123456789",
+                        dbUrl = "jdbc:sqlserver://localhost:1433;" +
+                                "databaseName=sales_storm;" +
+                                "encrypt=true;trustServerCertificate=true;";
+                conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+                System.out.println("Kết nối thành công");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
 }
