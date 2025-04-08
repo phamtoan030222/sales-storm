7f6b9e4077e934172dd0fe92cc1390a2cc38e235
Toan Pham <phamductoan030222@gmail.com>
2025-04-08 19:58:08 +0700

    Service

---
+++ src/javaapplication8/service/impl/NhanVienServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication8.service.impl;
+
+import javaapplication8.service.NhanVienService;
+
+/**
+ *
+ * @author phamd
+ */
+public class NhanVienServiceImpl implements NhanVienService{
+    
+}
